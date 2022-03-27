d1942cdf3e5d05d6e0219a8b2b872f1cfcc0a34f
crunchypi <crunchypi@protonmail.com>
2022-03-27 16:48:27 +0200

    knnRequest -> knnc.FilterStage

--- service/requestman/knnrequest.go
+++ service/requestman/knnrequest.go
@@ -1,10 +1,10 @@
 package requestman
 
 import (
-    "time"
+	"time"
 
-    "github.com/crunchypi/ddrop/pkg/knnc"
-    "github.com/crunchypi/ddrop/pkg/mathx"
+	"github.com/crunchypi/ddrop/pkg/knnc"
+	"github.com/crunchypi/ddrop/pkg/mathx"
 )
 
 /*
@@ -33,53 +33,53 @@ func (m *KNNMethod) Ok() bool {
 // KNNArgs are used as arguments for making KNN requests. Check if all the
 // requirements are met with calling KNNArgs.Ok().
 type KNNArgs struct {
-    // Namespace is used to group search spaces together, based on logical
-    // meaning, but also for having uniform vector dimensions.
+	// Namespace is used to group search spaces together, based on logical
+	// meaning, but also for having uniform vector dimensions.
 	Namespace string
-    // Priority specifies how important a KNN query is -- higher is better.
-    // It influences the number of goroutines used, though not necessarily
-    // a one-to-one mapping. Must be > 0.
-	Priority  int
-    // QueryVec is used for similarity searching. Must not be nil, with a
-    // length of > 0. Also, make sure the dimension is appropriate for the
-    // KNNArgs.namespace field.
-	QueryVec  []float64
-    // KNNMethod specifies the distance function used for the query.
-    // KNNMethod.Ok() must return true.
+	// Priority specifies how important a KNN query is -- higher is better.
+	// It influences the number of goroutines used, though not necessarily
+	// a one-to-one mapping. Must be > 0.
+	Priority int
+	// QueryVec is used for similarity searching. Must not be nil, with a
+	// length of > 0. Also, make sure the dimension is appropriate for the
+	// KNNArgs.namespace field.
+	QueryVec []float64
+	// KNNMethod specifies the distance function used for the query.
+	// KNNMethod.Ok() must return true.
 	KNNMethod KNNMethod
-    // Ascending plays a role with ordering _and_ the meaning is dependent
-    // somewhat on the KNNArgs.KNNMethod field.
-    //
-    // Euclidean distance, for instance, works on the principle that lower
-    // is better, so then it would make sense to have Ascending=true for
-    // KNN. For K-furthest-neighs, Ascending=false has to be used, as that
-    // would reverse the order. The exact opposite is true for Cosine simi.
+	// Ascending plays a role with ordering _and_ the meaning is dependent
+	// somewhat on the KNNArgs.KNNMethod field.
+	//
+	// Euclidean distance, for instance, works on the principle that lower
+	// is better, so then it would make sense to have Ascending=true for
+	// KNN. For K-furthest-neighs, Ascending=false has to be used, as that
+	// would reverse the order. The exact opposite is true for Cosine simi.
 	Ascending bool
-    // K is the K in KNN. However, the actual result might be less than this
-    // number, for multiple reasons. One of them is that there simply might
-    // not be enough data to search. Another reason is that the underlying
-    // knn pkg uses a few optimization tricks to trade accuracy for speed,
-    // the reamainding fields below give more documentation.
-	K         int
-    // Extent specifies the extent of a search, in a range (0, 1]. For
-    // example, 0.5 will search half the search space. This is used to
-    // trade accuracy for speed. 
-	Extent    float64
-    // Accept is another optimization trick; the search will be aborted
-    // when there are KNNArgs.K results with better than KNNArgs.Accept
-    // accuracy.
-	Accept    float64
-    // Reject is another optimization trick; the knn search pipeline will
-    // drop all values worse than this fairly early on, such that the
-    // load on downstream processes/pipes gets alleviated. Do note that
-    // this is evaluated before KNNArgs.Accept, so Accept can be cancelled
-    // out by Reject.
-	Reject    float64
-    // TTL specifies the deadline for a knn request. The pipeline will
-    // start shutting down for this request after the deadline, but it
-    // is a good idea to cancel it manually. After this duration, the
-    // best-found results are given. Must be > 0.
-	TTL       time.Duration
+	// K is the K in KNN. However, the actual result might be less than this
+	// number, for multiple reasons. One of them is that there simply might
+	// not be enough data to search. Another reason is that the underlying
+	// knn pkg uses a few optimization tricks to trade accuracy for speed,
+	// the reamainding fields below give more documentation.
+	K int
+	// Extent specifies the extent of a search, in a range (0, 1]. For
+	// example, 0.5 will search half the search space. This is used to
+	// trade accuracy for speed.
+	Extent float64
+	// Accept is another optimization trick; the search will be aborted
+	// when there are KNNArgs.K results with better than KNNArgs.Accept
+	// accuracy.
+	Accept float64
+	// Reject is another optimization trick; the knn search pipeline will
+	// drop all values worse than this fairly early on, such that the
+	// load on downstream processes/pipes gets alleviated. Do note that
+	// this is evaluated before KNNArgs.Accept, so Accept can be cancelled
+	// out by Reject.
+	Reject float64
+	// TTL specifies the deadline for a knn request. The pipeline will
+	// start shutting down for this request after the deadline, but it
+	// is a good idea to cancel it manually. After this duration, the
+	// best-found results are given. Must be > 0.
+	TTL time.Duration
 }
 
 // Ok checks if KNNArgs meets the minimum configuration requirement.
@@ -102,14 +102,13 @@ func (r *KNNArgs) Ok() bool {
 	return ok
 }
 
-
 // KNNEnqueueResult is used to receive the results of a KNN request/query.
 type KNNEnqueueResult struct {
-    // Pipe is the destination of a KNN request/query.
-	Pipe   chan knnc.ScoreItems
-    // Cancel can be used to cancel a request. Should be called when
-    // the deadline for a request (e.g KNNArgs.TTL is exceeded after
-    // a request is made).
+	// Pipe is the destination of a KNN request/query.
+	Pipe chan knnc.ScoreItems
+	// Cancel can be used to cancel a request. Should be called when
+	// the deadline for a request (e.g KNNArgs.TTL is exceeded after
+	// a request is made).
 	Cancel *knnc.CancelSignal
 }
 
@@ -118,21 +117,20 @@ type KNNEnqueueResult struct {
 // it is the type uses KNNArgs to create a knn result, which is sent
 // through knnRequest.enqueueResult.Pipe. Set it up with newKNNRequest.
 type knnRequest struct {
-	args     *KNNArgs
-    // Converted from args.QueryVec.
+	args *KNNArgs
+	// Converted from args.QueryVec.
 	queryVec mathx.Distancer
 	//----------------------------------------------------------------
 	// NOTE: For internal operations, these must be set for a query
 	// to be processed with the KNNRequest.process() method.
 	//----------------------------------------------------------------
 
-    // When the query is created. Will be used in combination with
-    // KNNArgs.TTL to know when to cancel a pipeline.
+	// When the query is created. Will be used in combination with
+	// KNNArgs.TTL to know when to cancel a pipeline.
 	created time.Time
 	// Destination of the request.
 	enqueueResult KNNEnqueueResult
 }
-
 
 // newKNNRequest is a convenience func for creating a knnRequest instance.
 // It sets up the internal KNNEnqueueResult instance safely and sets the
@@ -152,7 +150,6 @@ func newKNNRequest(args *KNNArgs) knnRequest {
 	}
 }
 
-
 // Ok checks if the instance meets the minimum safety requirements.
 // Returns true if:
 //  r.args.Ok(),
@@ -164,10 +161,9 @@ func (r *knnRequest) Ok() bool {
 	ok = ok && r.args.Ok()
 	ok = ok && r.enqueueResult.Cancel.Ok()
 	ok = ok && r.enqueueResult.Pipe != nil
-    ok = ok && r.created != time.Time{}
+	ok = ok && r.created != time.Time{}
 	return ok
 }
-
 
 /*
 --------------------------------------------------------------------------------
@@ -181,9 +177,11 @@ interact with the knnc package.
 // mapStageF is compatible with knnc.NewPipelineArgs.MapStage.
 type mapStageF = func(knnc.ScanChan) (<-chan knnc.ScoreItem, bool)
 
+// filterStage is compatible with knnc.NewPipelineArgs.FilterStage.
+type filterStageF = func(<-chan knnc.ScoreItem) (<-chan knnc.ScoreItem, bool)
 
 // toBaseWorkerArgs simply converts knnRequest into knnc.BaseWorkerArgs, using
-// some state from the internal knnRequest.args. Specifically: 
+// some state from the internal knnRequest.args. Specifically:
 //  Buf:    knnRequest.args.Priority
 //  Cancel: knnRequest.enqueueResult.Cancel
 //  TTL:    knnRequest.args.TTL - (time since knnRequest.created)
@@ -232,15 +230,52 @@ func (r *knnRequest) toMapFunc() func(other mathx.Distancer) (knnc.ScoreItem, bo
 	}
 }
 
-
 // toMapStage simply converts a knnRequest into a func that is compatible with
-// knnc.NewPipelineArgs.MapStage.
+// knnc.NewPipelineArgs.MapStage. It uses knnc.MapStage and constructs its args
+// with the following:
+//  - knnRequest.toBaseStageArgs()
+//  - knnRequest.toMapFunc()
 func (r *knnRequest) toMapStage() mapStageF {
 	return func(in knnc.ScanChan) (<-chan knnc.ScoreItem, bool) {
 		return knnc.MapStage(knnc.MapStageArgs{
 			In: in,
 			MapStagePartialArgs: knnc.MapStagePartialArgs{
 				MapFunc:       r.toMapFunc(),
+				BaseStageArgs: r.toBaseStageArgs(),
+			},
+		})
+	}
+}
+
+// toFilterFunc simply converts a knnRequest into a func that can be used with
+// knnc.FilterStagePartialArgs.FilterFunc. The returned func uses the internal
+// knnRequest.args.Reject to filter out scores 'worse' than score.Score. The
+// 'worse' part (higher/lower) is dependent on knnRequest.args.Ascending. To
+// give a clearer picture of the behavior:
+//  If Reject=1 and score.Score=2 and Ascending=true  -> return false
+//  If Reject=2 and score.Score=1 and Ascending=true  -> return true
+//
+// ... and flipping the Ascending flag gives the opposite results.
+func (r *knnRequest) toFilterFunc() func(score knnc.ScoreItem) bool {
+	return func(score knnc.ScoreItem) bool {
+		keep := false
+		keep = keep || score.Score < r.args.Reject && r.args.Ascending
+		keep = keep || score.Score > r.args.Reject && !r.args.Ascending
+		return keep
+	}
+}
+
+// toFilterStage simply converts a knnRequest into a func that is compatible with
+// knn.NewPipelineArgs.FilterStage. It uses knnc.FilterStage and constructs its
+// arguments with the following:
+//  - knnRequest.toBaseStageArgs()
+//  - knnRequest.toFilterFunc()
+func (r *knnRequest) toFilterStage() filterStageF {
+	return func(in <-chan knnc.ScoreItem) (<-chan knnc.ScoreItem, bool) {
+		return knnc.FilterStage(knnc.FilterStageArgs{
+			In: in,
+			FilterStagePartialArgs: knnc.FilterStagePartialArgs{
+				FilterFunc:    r.toFilterFunc(),
 				BaseStageArgs: r.toBaseStageArgs(),
 			},
 		})
--- service/requestman/knnrequest_test.go
+++ service/requestman/knnrequest_test.go
@@ -1,12 +1,14 @@
 package requestman
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/crunchypi/ddrop/pkg/knnc"
 	"github.com/crunchypi/ddrop/pkg/mathx"
 )
+
 /*
 --------------------------------------------------------------------------------
 Testing basics, i.e all the KNNRequest.toXYZ funcs that interact with the knnc
@@ -49,6 +51,73 @@ func TestKNNRequestToMapStage(t *testing.T) {
 
 	go func() {
 		chI <- knnc.ScanItem{Distancer: mathx.NewSafeVec(1, 2)}
+		close(chI)
+	}()
+
+	score := 0.
+	for scoreItem := range chO {
+		score += scoreItem.Score
+	}
+
+	if score == 0 {
+		t.Fatal("suspecting unset score")
+	}
+
+	if score != 1 {
+		t.Fatal("unexpected score:", score)
+	}
+}
+
+func TestKNNRequestToFilterFunc(t *testing.T) {
+	r := newKNNRequest(&KNNArgs{
+		Reject:    1.,
+		Ascending: true,
+	})
+
+	// Convenience.
+	fmtcfg := func() string {
+		return fmt.Sprintf("reject=%v, acending=%v",
+			r.args.Reject, r.args.Ascending)
+	}
+
+	// true = keep
+	if r.toFilterFunc()(knnc.ScoreItem{Score: 2}) {
+		t.Fatalf("cfg: '%v', dropped score=0", fmtcfg())
+	}
+
+	if !r.toFilterFunc()(knnc.ScoreItem{Score: 0}) {
+		t.Fatalf("cfg: '%v', kept score=0", fmtcfg())
+	}
+
+	// flip the significanse of scores.
+	r.args.Ascending = false
+	if !r.toFilterFunc()(knnc.ScoreItem{Score: 2}) {
+		t.Fatalf("cfg: '%v', dropped score=2", fmtcfg())
+	}
+
+	if r.toFilterFunc()(knnc.ScoreItem{Score: 0}) {
+		t.Fatalf("cfg: '%v', kept score=0", fmtcfg())
+	}
+}
+
+func TestKNNRequestToFilterStage(t *testing.T) {
+	r := newKNNRequest(&KNNArgs{
+		TTL:       time.Second,
+		Priority:  1,
+		Reject:    2,
+		Ascending: true,
+	})
+
+	chI := make(chan knnc.ScoreItem)
+	chO, ok := r.toFilterStage()(chI)
+	if !ok {
+		t.Fatal("failed starting filter stage")
+	}
+
+	go func() {
+		// On either side of qi.request.Reject.
+		chI <- knnc.ScoreItem{Score: 1}
+		chI <- knnc.ScoreItem{Score: 3}
 		close(chI)
 	}()
 
