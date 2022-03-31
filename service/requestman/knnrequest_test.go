6c6dddb65d0d50b61353b735792fcd7ee8b99187
crunchypi <crunchypi@protonmail.com>
2022-03-31 11:02:35 +0200

    more tests
    Added a test-util and a couple tests for varifying that some of
    the optimization parameters/args/fields in KNNRequest actually
    do increase performance.

--- service/requestman/knnrequest_test.go
+++ service/requestman/knnrequest_test.go
@@ -2,6 +2,7 @@ package requestman
 
 import (
 	"fmt"
+    "math/rand"
 	"testing"
 	"time"
 
@@ -269,5 +270,194 @@ func TestKNNRequestConsume(t *testing.T) {
 
 	if !set {
 		t.Fatal("didnt get any result")
+	}
+}
+
+/*
+--------------------------------------------------------------------------------
+Testing parameter tweaking. Some parameters/configs of KNNArgs are related to
+optimization and/or trading accuracy for speed. The tests below validate that
+(only the time component) by doing several runs while tweaking singular params
+(one param per test case/func), which is is conceptualised as a time series (ish)
+with a slope. The logic is that the (average) slope is negative if tweaks work.
+--------------------------------------------------------------------------------
+*/
+type testTimeSlopeArgs struct {
+	// Test data group.
+	poolSize int // Number of vecs in pool.
+	poolDim  int // Dimension of each vec in pool.
+
+	// Func behaviour group.
+	n int                  // Number of test iterations.
+	m int                  // Number of test iteration re-runs (evending out scores).
+	f func(i int) *KNNArgs // Func for getting KNNRequest. i is the curent iter of n.
+
+	// Note: f is called with changing i such that the KNNRequst can change in
+	// a way such that a query is faster/slower. m is just for evening out timestamps.
+}
+
+var scoreDump float64 // Just so the compiler won't do unwanted optimization.
+// Helper func intended for timeing queries while tweaking paramaters.
+// The purpose if to validate that some (query) param changes do indeed speed
+// up / slow down query times (accuracy is not checked). This is timed here and
+// the slope is returned.
+func testTimeSlope(args testTimeSlopeArgs) (time.Duration, bool) {
+	durations := make([]time.Duration, 0, args.n)
+
+	// Create data.
+	ss, _ := knnc.NewSearchSpaces(knnc.NewSearchSpacesArgs{
+		SearchSpacesMaxCap:      args.poolSize,
+		SearchSpacesMaxN:        args.poolSize,
+		MaintenanceTaskInterval: 1,
+	})
+
+	for i := 0; i < args.poolSize; i++ {
+		v, _ := mathx.NewSafeVecRand(args.poolDim)
+		ss.AddSearchable(&DistancerContainer{D: v})
+	}
+
+	// Loop for adjusting param.
+	for i := 0; i < args.n; i++ {
+		// Loop for average.
+		totalDuration := time.Duration(0)
+		for j := 0; j < args.m; j++ {
+			// Has to be in inner loop because the 'KNNEnqueueResult'
+			// instance cannot be re-used be design.
+			//request := args.f(i)
+			request := newKNNRequest(args.f(i))
+			if !request.Ok() {
+				return 0, false
+			}
+
+			stamp := time.Now()
+
+			// Query result might not have the requested k. Most correct would be
+			// to check "k == qi.request.K", but randomness is involved in pool
+			// creation (and the request from 'f(i)' has some freedom..). So leaving
+			// this as it is, at least it validates that the request went through.
+			go request.consume(ss)
+			if len((<-request.enqueueResult.Pipe).Trim()) == 0 {
+				return 0, false
+			}
+
+			delta := time.Now().Sub(stamp)
+			totalDuration += delta
+		}
+
+		averageDuration := totalDuration / time.Duration(args.m)
+		durations = append(durations, averageDuration)
+	}
+
+	// This method of calculation takes the average descent/ascent between
+	// each step as i approaches n. This is opposed to taking the average
+	// slope. There's no particular reason (the result is different but the
+	// negative/positive property is equivalent for this purpose), but it
+	// has the benefit of giving information about how discrete changes in i
+	// affects the performance (on average).
+	relativeDeltaSum := time.Duration(0)
+	for x := 0; x < len(durations)-1; x++ {
+		relativeDeltaSum += durations[x+1] - durations[x]
+	}
+	// -1 because it is relative deltas. 0 div potential but keeping it for
+	// simplicity, it's for testing anyway.
+	relativeDeltaAverage := relativeDeltaSum / (time.Duration(len(durations)) - 1)
+	return relativeDeltaAverage, true
+}
+
+func randFloat64Slice(dim int) ([]float64, bool) {
+	if dim <= 0 {
+		return nil, false
+	}
+
+	s := make([]float64, dim)
+	for i := 0; i < dim; i++ {
+		s[i] = rand.Float64()
+	}
+
+	return s, true
+}
+
+// Increases KNNRequest (search) 'priority' for each step, which should make
+// query faster because 'priority'=num of goroutines per stage.
+func TestTimeSlopePriority(t *testing.T) {
+	poolSize := 100_000
+	poolDim := 3
+
+	n := 5
+	m := 10
+
+	slope, ok := testTimeSlope(testTimeSlopeArgs{
+		poolSize: poolSize,
+		poolDim:  poolDim,
+		n:        n,
+		m:        m,
+		f: func(i int) *KNNArgs {
+			v, _ := randFloat64Slice(poolDim)
+
+			return &KNNArgs{
+				Namespace: "",
+				Priority:  i + 1, // +1 because 0 priority is invalid.
+				QueryVec:  v,
+				KNNMethod: KNNMethodCosineSimilarity,
+				Ascending: false,
+				K:         3,
+				Extent:    1,
+				Accept:    1,
+				Reject:    0,
+				TTL:       time.Minute,
+			}
+		},
+	})
+
+	if !ok {
+		t.Fatal("timeSlope func returned false, test is broken")
+	}
+	if slope > 0 {
+		t.Fatal("positive slope, implying increase in time per step.")
+	}
+}
+
+// Decreases KNNRequest (search) 'extent' for each step, which should make
+// query faster because a lower and lower amount of vec pool is checked.
+func TestTimeSlopeExtent(t *testing.T) {
+	poolSize := 2_000
+	poolDim := 3
+
+	n := 100
+	m := 10
+
+	slope, ok := testTimeSlope(testTimeSlopeArgs{
+		poolSize: poolSize,
+		poolDim:  poolDim,
+		n:        n,
+		m:        m,
+		f: func(i int) *KNNArgs {
+			v, _ := randFloat64Slice(poolDim)
+
+			// Increasing by n. Add constant to avoid 0 (KNNRequest.Ok() must pass).
+			step := (1. / float64(n) * float64(i)) + 0.000000001
+			// Decreasing by n.
+			extent := 1 - step
+
+			return &KNNArgs{
+				Namespace: "",
+				Priority:  1,
+				QueryVec:  v,
+				KNNMethod: KNNMethodCosineSimilarity,
+				Ascending: false,
+				K:         3,
+				Extent:    extent,
+				Accept:    1,
+				Reject:    0,
+				TTL:       time.Minute,
+			}
+		},
+	})
+
+	if !ok {
+		t.Fatal("timeSlope func returned false, test is broken")
+	}
+	if slope > 0 {
+		t.Fatal("positive slope, implying increase in time per step.")
 	}
 }
