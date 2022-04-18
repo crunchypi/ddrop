66d1075bed39d436acd6b2971bc8f8a320695369
crunchypi <crunchypi@protonmail.com>
2022-04-18 17:29:27 +0200

    add T timedLinkedList, did go fmt, and renamed a var.
    monitor.go (modified):
            - Renamed linkedListItem.data -> payload
            - Did go fmt
            - Added timedLinkedList. This is very similar to
              timex.LatencyTracker in that it is supposed to track
              things done over moving time windows. Here, it will be
              used to store monitoring data. Merging of the similar
              code is planned to be done at a later data, with a
              refactoring branch.
    
    monitor_test.go (modified)
            - Renamed linkedListItem.data -> payload
            - Did go fmt
            - Added a couple test cases for T timedLinkedList

--- service/requestman/monitor.go
+++ service/requestman/monitor.go
@@ -1,9 +1,19 @@
 package requestman
 
+import (
+	"time"
+)
+
+/*
+--------------------------------------------------------------------------------
+Linked list impl for tracking monitoring stats over windows of time.
+--------------------------------------------------------------------------------
+*/
+
 // linkedListItem is intended as a link in T linkedList.
 type linkedListItem[T any] struct {
-	data T
-	next *linkedListItem[T]
+	payload T
+	next    *linkedListItem[T]
 }
 
 // linkedList is a simple linked list implementation with a few useful methods.
@@ -27,7 +37,7 @@ func (ll *linkedList[T]) tail() (int, *linkedListItem[T]) {
 	var i int
 	var tail *linkedListItem[T]
 
-    // tail var above might not be set.
+	// tail var above might not be set.
 	ll.iter(func(j int, current *linkedListItem[T]) bool {
 		i = j
 		tail = current
@@ -38,11 +48,11 @@ func (ll *linkedList[T]) tail() (int, *linkedListItem[T]) {
 }
 
 // add adds a new link with the given data at the end of the linked list instance.
-func (ll *linkedList[T]) add(data T) {
-	newTail := linkedListItem[T]{data: data}
+func (ll *linkedList[T]) add(payload T) {
+	newTail := linkedListItem[T]{payload: payload}
 	_, tail := ll.tail()
 
-    // Handle nil/unset head.
+	// Handle nil/unset head.
 	if tail == nil {
 		ll.head = &newTail
 		return
@@ -51,13 +61,14 @@ func (ll *linkedList[T]) add(data T) {
 	tail.next = &newTail
 }
 
-// trim iterates over the linked list and passes each item (and index) to the 
+// trim iterates over the linked list and passes each item (and index) to the
 // receiving func. Will trim/delete the current item from the chain if the
 // receiving func returns false.
 func (ll *linkedList[T]) trim(f func(index int, item *linkedListItem[T]) bool) {
 	// Check head seperatly for cleanliness.
-	if ll.head != nil && !f(0, ll.head) {
+	if ll.head == nil || (ll.head != nil && !f(0, ll.head)) {
 		ll.head = nil
+		return
 	}
 
 	// Trim tail.
@@ -75,4 +86,91 @@ func (ll *linkedList[T]) trim(f func(index int, item *linkedListItem[T]) bool) {
 		curr = curr.next
 		iter++
 	}
+}
+
+// len returns the number of nodes.
+func (ll *linkedList[T]) len() int {
+	r := 0
+	ll.iter(func(_ int, _ *linkedListItem[T]) bool {
+		r++
+		return true
+	})
+
+	return r
+}
+
+// timed is a time wrapper for any T, i.e timed T.
+type timed[T any] struct {
+	created time.Time
+	inner   T
+}
+
+// timedLinkedList is an extension of T linkedList, where each link represents a
+// discrete frame of time. It's intended use case is to keep track of events during
+// windows of time, e.g latency the last second, minute, etc.
+//
+// Layout is [head:now]-...-[tail:then].
+type timedLinkedList[T any] struct {
+	inner linkedList[timed[T]]
+
+	// maxChainLinkN specifies the max amount of links in this chain.
+	// When the number of links exceeds this, and new links are added,
+	// then old ones (at the end/tail) are dropped.
+	maxChainLinkN int
+	// MinChainLinkSize represents the min time delta between any link.
+	// This linked list impl operates on the principle that each link
+	// represents a discrete timeframe, this field specifies that window.
+	minChainLinkSize time.Duration
+}
+
+// Inner exposes the inner linked list.
+func (tll *timedLinkedList[T]) Inner() *linkedList[timed[T]] {
+	return &tll.inner
+}
+
+// maintain does maintenance in order to make the instance state true to the
+// configurations. Specifically, it
+// - Adds a new head if current head is nil.
+// - Adds a new head (moving the old one) if delta time between now and
+//   creation time of old head is greater than tll.minChainLinkSize.
+// - Trims the tail such that n links does not exceed tll.maxChainLinkN
+func (tll *timedLinkedList[T]) maintain() {
+	// Handle nil head.
+	if tll.inner.head == nil {
+		tll.inner.add(timed[T]{created: time.Now()})
+	}
+
+	// Handle expired head.
+	headDelta := time.Now().Sub(tll.inner.head.payload.created)
+	if headDelta > tll.minChainLinkSize {
+		newHead := linkedListItem[timed[T]]{}
+		newHead.payload.created = time.Now()
+		newHead.next = tll.inner.head
+		tll.inner.head = &newHead
+	}
+
+	// Handle tail limit.
+	tll.inner.trim(func(i int, item *linkedListItem[timed[T]]) bool {
+		return i < tll.maxChainLinkN
+	})
+}
+
+// timeRange returns all links that were created since time.Now() - period.
+// So period=time.Minute will return all nodes creted the last minute.
+func (tll *timedLinkedList[T]) timeRange(period time.Duration) []timed[T] {
+	stamp := time.Now()
+	tll.maintain()
+
+	result := make([]timed[T], 0, tll.maxChainLinkN)
+	tll.inner.trim(func(i int, item *linkedListItem[timed[T]]) bool {
+		include := stamp.Sub(item.payload.created) <= period
+
+		if include {
+			result = append(result, item.payload)
+		}
+
+		return include
+	})
+
+	return result
 }
--- service/requestman/monitor_test.go
+++ service/requestman/monitor_test.go
@@ -2,40 +2,47 @@ package requestman
 
 import (
 	"testing"
+	"time"
 )
+
+/*
+--------------------------------------------------------------------------------
+Tests for linked list.
+--------------------------------------------------------------------------------
+*/
 
 func TestLLIter(t *testing.T) {
 	nItems := 5
 
-	// Add 'nItems' linked items, where data=index.
-	ll := linkedList[int]{head: &linkedListItem[int]{data: 0}}
+	// Add 'nItems' linked items, where payload=index.
+	ll := linkedList[int]{head: &linkedListItem[int]{payload: 0}}
 	current := ll.head
 	for i := 1; i < nItems; i++ {
-		current.next = &linkedListItem[int]{data: i}
+		current.next = &linkedListItem[int]{payload: i}
 		current = current.next
 	}
 
-	// Set this to the 'data' field of tail
+	// Set this to the 'payload' field of tail
 	lastData := 0
 	ll.iter(func(_ int, item *linkedListItem[int]) bool {
-		lastData = item.data
+		lastData = item.payload
 		return true
 	})
 
 	// -1 because zero indexed.
 	if lastData != (nItems - 1) {
-		t.Fatal("unexpected last data item from linked list:", lastData)
+		t.Fatal("unexpected last payload item from linked list:", lastData)
 	}
 }
 
 func TestLLTail(t *testing.T) {
 	nItems := 5
 
-	// Add 'nItems' linked items, where data=index.
-	ll := linkedList[int]{head: &linkedListItem[int]{data: 0}}
+	// Add 'nItems' linked items, where payload=index.
+	ll := linkedList[int]{head: &linkedListItem[int]{payload: 0}}
 	current := ll.head
 	for i := 1; i < nItems; i++ {
-		current.next = &linkedListItem[int]{data: i}
+		current.next = &linkedListItem[int]{payload: i}
 		current = current.next
 	}
 
@@ -46,8 +53,8 @@ func TestLLTail(t *testing.T) {
 		t.Fatal("unexpected tail index:", tailIndex)
 	}
 	// -1 because zero indexed.
-	if tail.data != (nItems - 1) {
-		t.Fatal("unexpected tail data:", tail.data)
+	if tail.payload != (nItems - 1) {
+		t.Fatal("unexpected tail payload:", tail.payload)
 	}
 }
 
@@ -74,25 +81,25 @@ func TestLLAdd(t *testing.T) {
 	}
 
 	// -1 because zero indexed.
-	if current.data != (nItems - 1) {
-		t.Fatal("unexpected tail data:", current.data)
+	if current.payload != (nItems - 1) {
+		t.Fatal("unexpected tail payload:", current.payload)
 	}
 }
 
 func TestLLTrim(t *testing.T) {
 	nItems := 5
 
-	// Add 'nItems' linked items, where data=index.
-	ll := linkedList[int]{head: &linkedListItem[int]{data: 0}}
+	// Add 'nItems' linked items, where payload=index.
+	ll := linkedList[int]{head: &linkedListItem[int]{payload: 0}}
 	current := ll.head
 	for i := 1; i < nItems; i++ {
-		current.next = &linkedListItem[int]{data: i}
+		current.next = &linkedListItem[int]{payload: i}
 		current = current.next
 	}
 
-	// Trim everything besides item.data == 0, i.e keep only head.
+	// Trim everything besides item.payload == 0, i.e keep only head.
 	ll.trim(func(index int, item *linkedListItem[int]) bool {
-		return item.data == 0
+		return item.payload == 0
 	})
 
 	tailIndex, tail := ll.tail()
@@ -101,7 +108,77 @@ func TestLLTrim(t *testing.T) {
 	if tailIndex != 0 {
 		t.Fatal("unexpected tail index:", tailIndex)
 	}
-	if tail.data != 0 {
-		t.Fatal("unexpected tail data:", tail.data)
+	if tail.payload != 0 {
+		t.Fatal("unexpected tail payload:", tail.payload)
+	}
+}
+
+func TestLLLen(t *testing.T) {
+	n := 9
+	ll := linkedList[int]{}
+	for i := 0; i < n; i++ {
+		ll.add(i)
+	}
+
+	if ll.len() != n {
+		t.Fatal("unexpected len:", ll.len())
+	}
+}
+
+func TestTLLMaintain(t *testing.T) {
+	maxN := 10
+	minD := time.Millisecond * 10
+
+	tll := timedLinkedList[int]{
+		inner:            linkedList[timed[int]]{},
+		maxChainLinkN:    maxN,
+		minChainLinkSize: minD,
+	}
+
+	// Setting head.
+	tll.maintain()
+	if tll.Inner().len() != 1 {
+		t.Fatal("didn't set head")
+	}
+
+	// New head, so len must be 2.
+	time.Sleep(minD)
+	tll.maintain()
+	if tll.Inner().len() != 2 {
+		t.Fatal("could not add to head")
+	}
+
+	// Add many, but excess should be trimmed.
+	for i := 0; i < maxN*2; i++ {
+		tll.maintain()
+		time.Sleep(minD)
+	}
+
+	if tll.Inner().len() != maxN {
+		t.Fatal("unexpected len after trim:", tll.Inner().len())
+	}
+}
+
+func TestTLLTimeRange(t *testing.T) {
+	maxN := 6
+	minD := time.Second
+
+	tll := timedLinkedList[int]{
+		inner:            linkedList[timed[int]]{},
+		maxChainLinkN:    maxN,
+		minChainLinkSize: minD,
+	}
+
+	stamp := time.Now()
+	for i := 0; i < maxN; i++ {
+		tll.maintain()
+		time.Sleep(minD)
+	}
+
+	halfStamp := time.Now().Sub(stamp) / 2
+	links := tll.timeRange(halfStamp)
+
+	if len(links) != maxN/2 {
+		t.Fatal("unexpected result len:", len(links))
 	}
 }
