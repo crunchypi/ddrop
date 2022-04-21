d3e32a7a1037dc5424c94836b531aa41c0052840
crunchypi <crunchypi@protonmail.com>
2022-04-21 22:28:43 +0200

    started impl proper for monitor + tests
    monitor.go (mod):
            Added monitoring items (containing knn request stats) and
            the main monitoring type, which uses the timedLinkedList
            impl added a couple commits ago.
    
            The monitoring impl isn't complete, it has:
                    Method for merging monitoring items into the
                    linked list, which is a bit tricky.
                    - mon.registerMonItem(...)
                    Getting overall stats for a given period.
                    - mon.average(...)
            It currently lacks a way of actually registering
            knn requests, the aforementioned is the setup for that.
    
    monitor_test.go (mod):
            Added tests for the aforementioned types and methods.

--- service/requestman/monitor.go
+++ service/requestman/monitor.go
@@ -1,6 +1,8 @@
 package requestman
 
 import (
+	"math"
+	"sync"
 	"time"
 )
 
@@ -188,5 +190,164 @@ func (tll *timedLinkedList[T]) timeRange(start, end time.Time) []timed[T] {
 	})
 
 	tll.maintain()
+	return result
+}
+
+/*
+--------------------------------------------------------------------------------
+Monitor impl starts here.
+--------------------------------------------------------------------------------
+*/
+
+// knnMonItem captures stats per individual KNN request.
+type knnMonItem struct {
+	Latency      time.Duration
+	AvgScore     float64
+	Satisfaction float64
+}
+
+// KNNMonItemAvg captures stats for a group of KNN requests over a period.
+type KNNMonItemAvg struct {
+	isSet   bool
+	Created time.Time     // Start of recording.
+	Span    time.Duration // Recording duration.
+
+	N               int           // Number of recorded requests (including fails).
+	NFailed         int           // Number of (completely) failed requests.
+	AvgLatency      time.Duration // Average latency of all requests.
+	AvgScore        float64       // Average score for all requests.
+	AvgScoreNoFails float64       // Same as AvgScore but without fails.
+	AvgSatisfaction float64       // Success ratio (got n / want n).
+}
+
+// mergeKNNMonItem merges a knnMonItem in such a way that averages are maintained.
+// Note that KNNMonItemAvg.AvgScoreNoFails will have some imprecision and should
+// only be used for estimation.
+//
+// Internals: ia.isSet will be set to true.
+func (ia *KNNMonItemAvg) mergeKNNMonItem(i knnMonItem) {
+	if !ia.isSet {
+		ia.isSet = true
+		ia.Created = time.Now()
+	}
+
+	// Expand to old total.
+	n := float64(ia.N)
+	totalLatency := ia.AvgLatency * time.Duration(n)
+	totalScore := ia.AvgScore * n
+	totalSatisfaction := ia.AvgSatisfaction * n
+
+	// Add and contract to new average. Note const to prevent zero div.
+	c := 0.00000001
+	n++
+	ia.N = int(n)
+	ia.NFailed += int(math.Floor(1 - i.Satisfaction))
+	ia.AvgLatency = (time.Duration(totalLatency) + i.Latency) / time.Duration(n)
+	ia.AvgScore = (totalScore + i.AvgScore) / n
+	ia.AvgScoreNoFails = (totalScore + i.AvgScore) / (n - float64(ia.NFailed) + c)
+	ia.AvgSatisfaction = (totalSatisfaction + i.Satisfaction) / n
+}
+
+// mergeKNNMonItemAvg merges another KNNMonItemAvg instance with this instance,
+// only 'this' is changed. The merging is done as follows:
+// - this.Created is set to be the oldest.
+// - other.N is added to this.N.
+// - All other field pairs are simply added, divided by 2, then set to this.
+func (ia *KNNMonItemAvg) mergeKNNMonItemAvg(other *KNNMonItemAvg) {
+	if !ia.isSet {
+		*ia = *other
+		return
+	}
+	if !other.isSet {
+		return
+	}
+
+	// Set to earliest timestamp.
+	if other.Created.Before(ia.Created) {
+		ia.Created = other.Created
+	}
+
+	ia.Span = (ia.Span + other.Span) / 2
+	ia.N = ia.N + other.N
+	ia.NFailed = (ia.NFailed + other.NFailed)
+	ia.AvgLatency = (ia.AvgLatency + other.AvgLatency) / 2
+	ia.AvgScore = (ia.AvgScore + other.AvgScore) / 2
+	ia.AvgScoreNoFails = (ia.AvgScoreNoFails + other.AvgScoreNoFails) / 2
+	ia.AvgSatisfaction = (ia.AvgSatisfaction + other.AvgSatisfaction) / 2
+}
+
+// knnMonitor is intended for monitoring KNN requests in this pkg. It operates
+// on the principle that current entries are added at the head of a linked list,
+// and over time the entries are pushed towards the tail. This gives averages
+// of monitoring items (T KNNMonItemAvg) in particular time frames.. The entries
+// themselves are registered with:
+//  knnMonitor.register(...)
+// The method accepts a KNNEnqueueResults, which is intended to be what a KNN
+// requester gets after making a query. That T contains a chan with results;
+// the idea here is to put a 'man in the middle' listener between the user
+// and the sender end of that chan, then capture the results that way.
+// Intended setup:
+// - User makes a knn request.
+// - A instance of KNNEnqueueResult is made, by default it goes to both the
+//   requester and the internal request processing (latter sends results to
+//   the former).
+// - That KNNEnqueueResult (A) is put into knnMonitor.register(...), which
+//   returns another KNNEnqueueResult (B).
+// - Internal request processing gets A, requester gets B.
+// - Read with knnMonitor.average(...)
+//
+// Note; thread safe.
+type knnMonitor struct {
+	mx       sync.Mutex
+	averages *timedLinkedList[KNNMonItemAvg]
+}
+
+// registerMonItem merges a knnMonItem into the head of the internal linked list.
+func (m *knnMonitor) registerMonItem(item knnMonItem) {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+
+	// Garantee head.
+	m.averages.maintain()
+	monItem := &m.averages.inner.head.payload.inner
+	if !monItem.isSet {
+		monItem.isSet = true
+		monItem.Created = m.averages.inner.head.payload.created
+		monItem.Span = m.averages.minChainLinkSize
+	}
+
+	monItem.mergeKNNMonItem(item)
+}
+
+// average merges together all internal KNNMonItemAvg in the given period,
+// then returns the result. Note that 'start' should be _after_ 'end', this
+// might be counter-intuitive. The reason being that the underlying linked
+// list goes from head to tail in a reverse chronological order, so the
+// layout is [now-x*0]-[now-x*1]-[now-x*2] etc.
+//
+// Example: to get items created the last minute, one should do this:
+//  now := time.Now()
+//  x.timeRange(now, now.Add(-time.Minute))
+//
+// Note; thread safe.
+func (m *knnMonitor) average(start, end time.Time) KNNMonItemAvg {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+
+	items := m.averages.timeRange(start, end)
+	if len(items) == 0 {
+		return KNNMonItemAvg{}
+	}
+
+	result := items[0].inner
+	items = items[1:]
+	for _, itemAvg := range items {
+		if !itemAvg.inner.isSet {
+			continue
+		}
+		result.mergeKNNMonItemAvg(&itemAvg.inner)
+	}
+
+	m.averages.maintain()
 	return result
 }
--- service/requestman/monitor_test.go
+++ service/requestman/monitor_test.go
@@ -4,6 +4,8 @@ import (
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/crunchypi/ddrop/pkg/mathx"
 )
 
 func init() {
@@ -186,5 +188,150 @@ func TestTLLTimeRange(t *testing.T) {
 
 	if len(links) != int(float64(maxN)*span) {
 		t.Fatal("unexpected result len:", len(links))
+	}
+}
+
+/*
+--------------------------------------------------------------------------------
+Tests for monitor.
+--------------------------------------------------------------------------------
+*/
+
+func TestMonItemAvgMergeKNNMonItem(t *testing.T) {
+	kmi1 := knnMonItem{Latency: 1, AvgScore: 0.5, Satisfaction: 0.4}
+	kmi2 := knnMonItem{Latency: 1, AvgScore: 0.0, Satisfaction: 0.0}
+
+	kmia := KNNMonItemAvg{}
+	kmia.mergeKNNMonItem(kmi1)
+	kmia.mergeKNNMonItem(kmi2)
+	kmia.AvgScoreNoFails = mathx.RoundF64(kmia.AvgScoreNoFails, 2)
+
+	if kmia.N != 2 {
+		t.Fatal("unexpected N:", kmia.N)
+	}
+	if kmia.NFailed != 1 {
+		t.Fatal("unexpected NFailed:", kmia.NFailed)
+	}
+	if kmia.AvgLatency != (kmi1.Latency+kmi2.Latency)/2 {
+		t.Fatal("unexpected AvgLatency:", kmia.AvgLatency)
+	}
+	if kmia.AvgScore != (kmi1.AvgScore+kmi2.AvgScore)/2 {
+		t.Fatal("unexpected AvgScore:", kmia.AvgScore)
+	}
+	if kmia.AvgScoreNoFails != kmi1.AvgScore {
+		t.Fatal("unexpected AvgScoreNoFails:", kmia.AvgScoreNoFails)
+	}
+	if kmia.AvgSatisfaction != (kmi1.Satisfaction+kmi2.Satisfaction)/2 {
+		t.Fatal("unexpected AvgSatisfaction:", kmia.AvgSatisfaction)
+	}
+}
+
+func TestMonItemAvgMergeKNNMonItemAvg(t *testing.T) {
+	n := 10 // Must be even.
+
+	knnMonItems := make([]knnMonItem, n)
+	for i := 0; i < n; i++ {
+		knnMonItems[i] = knnMonItem{
+			Latency:      time.Millisecond * time.Duration(rand.Int63n(10)),
+			AvgScore:     rand.Float64(),
+			Satisfaction: rand.Float64(),
+		}
+	}
+
+	kmiaExpect := KNNMonItemAvg{}
+	for _, item := range knnMonItems {
+		kmiaExpect.mergeKNNMonItem(item)
+	}
+
+	half1 := knnMonItems[:n]
+	kmia1 := KNNMonItemAvg{}
+	for _, item := range half1 {
+		kmia1.mergeKNNMonItem(item)
+	}
+
+	half2 := knnMonItems[n:]
+	kmia2 := KNNMonItemAvg{}
+	for _, item := range half2 {
+		kmia2.mergeKNNMonItem(item)
+	}
+
+	kmiaCombo := KNNMonItemAvg{}
+	kmiaCombo.mergeKNNMonItemAvg(&kmia1)
+	kmiaCombo.mergeKNNMonItemAvg(&kmia2)
+
+	if kmiaCombo.Created != kmia1.Created {
+		s := "unexpected time; want %v, have %v \n"
+		t.Fatalf(s, kmia1.Created, kmiaCombo.Created)
+	}
+
+	if kmiaCombo.N != kmiaExpect.N {
+		s := "unexpected N field val; want %v, got %v\n"
+		t.Fatalf(s, kmiaExpect.N, kmiaCombo.N)
+	}
+
+	if kmiaCombo.NFailed != kmiaExpect.NFailed {
+		s := "unexpected NFailed field val; want %v, got %v\n"
+		t.Fatalf(s, kmiaExpect.NFailed, kmiaCombo.NFailed)
+	}
+
+	if kmiaCombo.AvgLatency != kmiaExpect.AvgLatency {
+		s := "unexpected AvgLatency field val; want %v, got %v\n"
+		t.Fatalf(s, kmiaExpect.AvgLatency, kmiaCombo.AvgLatency)
+	}
+
+	if kmiaCombo.AvgScore != kmiaExpect.AvgScore {
+		s := "unexpected AvgScore field val; want %v, got %v\n"
+		t.Fatalf(s, kmiaExpect.AvgScore, kmiaCombo.AvgScore)
+	}
+
+	if kmiaCombo.AvgScoreNoFails != kmiaExpect.AvgScoreNoFails {
+		s := "unexpected AvgScoreNoFails field val; want %v, got %v\n"
+		t.Fatalf(s, kmiaExpect.AvgScoreNoFails, kmiaCombo.AvgScoreNoFails)
+	}
+
+	if kmiaCombo.AvgSatisfaction != kmiaExpect.AvgSatisfaction {
+		s := "unexpected AvgSatisfaction field val; want %v, got %v\n"
+		t.Fatalf(s, kmiaExpect.AvgSatisfaction, kmiaCombo.AvgSatisfaction)
+	}
+}
+
+func TestMonitorAveragePrecice(t *testing.T) {
+	d := time.Millisecond * 100
+	testStarted := time.Now()
+
+	kmi1 := knnMonItem{Latency: 1, AvgScore: 0.0, Satisfaction: 0.0}
+	kmi2 := knnMonItem{Latency: 1, AvgScore: 0.5, Satisfaction: 0.4}
+
+	monitor := knnMonitor{averages: &timedLinkedList[KNNMonItemAvg]{
+		maxChainLinkN:    10,
+		minChainLinkSize: d,
+	}}
+
+	// ll layout, starting with head:  [kmi2]-[kmi1]-[kmi1x2]
+	monitor.registerMonItem(kmi1)
+	monitor.registerMonItem(kmi1)
+	time.Sleep(d)
+	monitor.registerMonItem(kmi1)
+	time.Sleep(d)
+	monitor.registerMonItem(kmi2)
+
+	// All nodes.
+	r := monitor.average(testStarted, testStarted.Add(-time.Hour))
+	// Note, not checking all stats, that is done in one of the tests above,
+	// related to KNNMonItemAvg.
+	if r.N != 4 {
+		t.Fatal("unexpected N field val:", r.N)
+	}
+	if r.AvgScore != 0.125 {
+		t.Fatal("unexpected AvgScore field val:", r.AvgScore)
+	}
+
+	// 2/3 nodes in the linked list (so kmi1+kmi2) and take their average.
+	r = monitor.average(testStarted, testStarted.Add(-d*2))
+	if r.N != 2 {
+		t.Fatal("unexpected N field val:", r.N)
+	}
+	if r.AvgScore != 0.25 {
+		t.Fatal("unexpected AvgScore field val:", r.AvgScore)
 	}
 }
